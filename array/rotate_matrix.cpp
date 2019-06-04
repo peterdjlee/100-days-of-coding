ce25bd09098d7217ea686579d797e20bd2c55159
Peter Lee <peterdjlee@gmail.com>
2019-06-04 18:15:10 -0400

    solve zero_matrix.cpp

--- array/rotate_matrix.cpp
+++ array/rotate_matrix.cpp
@@ -1,3 +1,10 @@
+// Problem from Cracking the Coding Interview: Chapter 1
+// 1.7
+// Rotate Matrix: Given an image represented by an NxN matrix, 
+// where each pixel in the image is represented by an integer,
+// write a method to rotate the image by 90 degrees.
+// Can you do this in place?
+
 #include <iostream>
 #include <time.h>
 
